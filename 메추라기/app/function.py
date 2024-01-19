06744e33888f69ed8b58248d174662257f2721e6
Chaosfire7 <kland2001@naver.com>
2024-01-19 21:19:00 +0900

    [add] register and login api (code by wikidocs)

--- 메추라기/app/function.py
+++ 메추라기/app/function.py
@@ -4,29 +4,32 @@ client = OpenAI(
     api_key=OPENAI_API_KEY,
 )
 
-response = client.chat.completions.create(
-  model="gpt-4-vision-preview",
-  messages=[
-    {"role": "system", "content": '''You are a person who scores from 1 to 10 for art and creativity
-                                    below is example
-                                    art : 5
-                                    creativity : 8'''},
-    {
-      "role": "user",
-      "content": [
-        {"type": "text", "text": '''What is this image? and Give me a score'''},
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+def imageToPrompt(dir:str)->str:
+  response = client.chat.completions.create(
+    model="gpt-4-vision-preview",
+    messages=[
+      {"role": "system", "content": '''You are a child psychotherapy ai. You can score joy, sadness, anguish, and excitement from 1 to 10 points
+                                      joy : 5
+                                      sadness : 8
+                                      anguish : 6
+                                      excitement : 4'''},
+      {
+        "role": "user",
+        "content": [
+          {"type": "text", "text": '''Give me a score'''},
+          {
+            "type": "image_url",
+            "image_url": {
+              "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+            },
           },
-        },
-      ],
-    }
-  ],
-  max_tokens=300,
-)
+        ],
+      }
+    ],
+    max_tokens=300,
+  )
 
-print(response.choices[0])
-print()
-print(response.choices[0].message.content)
+  return response.choices[0].message.content
+
+def promptToResult(prompt : str) -> list:
+  pass
